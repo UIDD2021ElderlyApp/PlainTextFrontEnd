b83f41e683baec35307682d05ea83c3e0d18bff6
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-05-07 02:58:26 +0800

    poop post ok

--- first_test/index.js
+++ first_test/index.js
@@ -1,3 +1,16 @@
-import { init } from 'init.js';
+var DEF_DEBUG = true;
+var DEF_NO_HTML_DISP = false;
+
+function init() {
+    if (DEF_DEBUG) {
+        console.log("dummiesTest success");
+    }else{
+        DEF_NO_HTML_DISP = true;
+    }
+    if (DEF_NO_HTML_DISP) {
+        document.getElementById("dont_show_this_in_published_client").style.display = "none";
+    }
+
+}
 
 init();
